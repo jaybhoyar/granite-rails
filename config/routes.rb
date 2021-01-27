c7f3741dca18dca8e9ecf64d5fbabd6976b8b7e7
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-01-27 14:58:37 +0530

    Added migrations for tasks table

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
+  root to: "home#welcome"
+  get "/articles" => "articles#list"
+  resource :profile
+  resources :books
 end
