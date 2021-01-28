0d445eef6a2a910966df439a1a230f9cb28a1263
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-01-28 10:43:33 +0530

    Setup tailwindcss

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_092225) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "name"
-    t.text "desc"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_01_27_110714) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "title"
