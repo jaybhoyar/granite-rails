090f5e0b8fd57eead5c683975daf78e1750e4267
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-01-29 15:23:24 +0530

    Created Toastr component and added axios interceptors

--- app/javascript/src/App.js
+++ app/javascript/src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
 import Dashboard from "components/Dashboard";
 import CreateTask from "components/Tasks/CreateTask";
-
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import PageLoader from "components/PageLoader";
+import { registerIntercepts, setAuthHeaders } from "apis/axios";
+import { initializeLogger } from "common/logger";
 const App = () => {
+  // const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    initializeLogger();
+    registerIntercepts();
+    setAuthHeaders(setLoading);
+  }, []);
+
+  // if (loading) {
+  //   return (
+  //     <div className="h-screen">
+  //       <PageLoader />
+  //     </div>
+  //   );
+  // }
   return (
     <Router>
+      <ToastContainer />
       <Switch>
         <Route exact path="/tasks/create" component={CreateTask} />
         <Route exact path="/dashboard" component={Dashboard} />
--- app/javascript/src/apis/axios.js
+++ app/javascript/src/apis/axios.js
@@ -3,3 +3,35 @@ axios.defaults.headers = {
   Accept: "applicaion/json",
   "Content-Type": "application/json",
 };
+const handleSuccessResponse = (response) => {
+  if (response) {
+    response.success = response.status === 200;
+    if (response.data.notice) {
+      Toastr.success(response.data.notice);
+    }
+  }
+  return response;
+};
+
+const handleErrorResponse = (error) => {
+  if (error.response?.status === 401) {
+    setToLocalStorage({ authToken: null, email: null, userId: null });
+  }
+  Toastr.error(
+    error.response?.data?.error ||
+      error.response?.data?.notice ||
+      error.message ||
+      error.notice ||
+      "Something went wrong!"
+  );
+  if (error.response?.status === 423) {
+    window.location.href = "/";
+  }
+  return Promise.reject(error);
+};
+
+export const registerIntercepts = () => {
+  axios.interceptors.response.use(handleSuccessResponse, (error) =>
+    handleErrorResponse(error)
+  );
+};
